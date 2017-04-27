05c3095dfcfb75b302f16d972ff5c7f7135fbf2d
Phil Gates-Shannon <phil.gs@gmail.com>
2017-04-27 18:36:56 -0400

    Move calculations back to Streamograph component and pass down variables as props

--- src/Streamograph.jsx
+++ src/Streamograph.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as d3 from 'd3';
 
+let priv = {};
+
 class Streamograph extends React.Component {
 
     shouldComponentUpdate() {
@@ -18,22 +20,89 @@ class Streamograph extends React.Component {
      * https://bl.ocks.org/mbostock/4060954
      */
     drawGraph() {
-        const svg = d3.select( "svg" );
+        priv.stack = d3.stack()
+            .keys( d3.range( this.props.n ) )
+            .offset( d3.stackOffsetWiggle );
 
-        svg.selectAll("path")
-            .data( this.props.layers )
-            .enter().append("path")
-            .attr( "d", this.props.area )
-            .attr( "fill", () => this.props.z( Math.random() ) );
+        priv.displayLayer = priv.stack(
+            d3.transpose(
+                d3.range( this.props.n )
+                    .map( () => bumps( this.props.m, this.props.k ) )
+            )
+        );
+        priv.hiddenLayer = priv.stack(
+            d3.transpose(
+                d3.range( this.props.n )
+                    .map( () => bumps( this.props.m, this.props.k ) )
+            )
+        );
+        priv.allLayers = priv.displayLayer.concat( priv.hiddenLayer );
 
-        // function transition() {
-        //     let t;
-        //     d3.selectAll("path")
-        //         .data((t = layers1, layers1 = layers0, layers0 = t))
-        //         .transition()
-        //         .duration(2500)
-        //         .attr("d", area);
-        // }
+        const svg = d3.select( this.svg );
+
+        const x = d3.scaleLinear()
+            .domain( [ 0, this.props.m - 1 ] )
+            .range( [ 0, this.props.width ] );
+
+        const y = d3.scaleLinear()
+            .domain( [
+                d3.min( priv.allLayers, stackMin ),
+                d3.max( priv.allLayers, stackMax )
+            ] )
+            .range( [ this.props.height, 0 ] );
+
+        const z = d3.interpolateCool;
+
+        const area = d3.area()
+            .x( ( d, i ) => x( i ) )
+            .y0( d => y( d[ 0 ] ) )
+            .y1( d => y( d[ 1 ] ) );
+
+        svg.selectAll( 'path' )
+            .data( priv.displayLayer )
+            .enter().append( 'path' )
+            .attr( 'd', area )
+            .attr( 'fill', () => z( Math.random() ) );
+
+        function stackMax( layer ) {
+            return d3.max( layer, d => d[ 1 ] );
+        }
+
+        function stackMin( layer ) {
+            return d3.min( layer, d => d[ 0 ] );
+        }
+
+        function transition() {
+            let t;
+            t = priv.hiddenLayer;
+            priv.hiddenLayer = priv.displayLayer;
+            priv.displayLayer = t;
+
+            d3.selectAll( 'path' )
+                .data( priv.displayLayer )
+                .transition()
+                .duration( 2500 )
+                .attr( 'd', area );
+        }
+
+        // Inspired by Lee Byron’s test data generator.
+        function bumps( n, m ) {
+            const a = [];
+            let i;
+            for ( i = 0; i < n; ++i ) a[ i ] = 0;
+            for ( i = 0; i < m; ++i ) bump( a, n );
+            return a;
+        }
+
+        function bump( a, n ) {
+            const x = 1 / (0.1 + Math.random()),
+                y = 2 * Math.random() - 0.5,
+                z = 10 / (0.1 + Math.random());
+            for ( let i = 0; i < n; i++ ) {
+                let w = (i / n - y) * z;
+                a[ i ] += x * Math.exp( -w * w );
+            }
+        }
     }
 
     render() {
@@ -42,8 +111,8 @@ class Streamograph extends React.Component {
                 <p>Streamograph</p>
                 <svg
                     ref={ element => { this.svg = element } }
-                    width="960"
-                    height="500"
+                    width={ this.props.width }
+                    height={ this.props.height }
                 />
             </div>
         );
@@ -51,11 +120,11 @@ class Streamograph extends React.Component {
 }
 
 Streamograph.propTypes = {
-    area: PropTypes.any.isRequired,
     height: PropTypes.number.isRequired,
-    layers: PropTypes.array.isRequired,
-    width: PropTypes.number.isRequired,
-    z: PropTypes.func.isRequired
+    k: PropTypes.number.isRequired,
+    m: PropTypes.number.isRequired,
+    n: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired
 };
 
 export default Streamograph;
--- src/index.jsx
+++ src/index.jsx
@@ -1,87 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import * as d3 from 'd3';
-import _ from 'lodash';
 import Streamograph from './Streamograph';
 import './stylesheets/app.scss';
 
-// Inspired by Lee Byron’s test data generator.
-const bumps = function(n, m) {
-    const a = [];
-    let i;
-    for (i = 0; i < n; ++i) a[i] = 0;
-    for (i = 0; i < m; ++i) bump(a, n);
-    return a;
-};
-
-const bump = function(a, n) {
-    const x = 1 / (0.1 + Math.random()),
-        y = 2 * Math.random() - 0.5,
-        z = 10 / (0.1 + Math.random());
-    for ( let i = 0; i < n; i++) {
-        let w = (i / n - y) * z;
-        a[i] += x * Math.exp(-w * w);
-    }
-};
-
-const generateStreamData = function( width, height) {
-    const n = 20, // number of layers
-        m = 200, // number of samples per layer
-        k = 10; // number of bumps per layer
-
-    const stack = d3.stack().keys( d3.range( n ) ).offset( d3.stackOffsetWiggle );
-    let layers0 = stack( d3.transpose( d3.range( n ).map( function() {
-            return bumps( m, k );
-        } ) ) ),
-        layers1 = stack( d3.transpose( d3.range( n ).map( function() {
-            return bumps( m, k );
-        } ) ) );
-    const layers = layers0.concat( layers1 );
-
-    const x = d3.scaleLinear()
-        .domain( [ 0, m - 1 ] )
-        .range( [ 0, width ] );
-
-    const y = d3.scaleLinear()
-        .domain( [ d3.min( layers, stackMin ), d3.max( layers, stackMax ) ] )
-        .range( [ height, 0 ] );
-
-    const z = d3.interpolateCool;
-
-    const area = d3.area()
-        .x( function( d, i ) {
-            return x( i );
-        } )
-        .y0( function( d ) {
-            return y( d[ 0 ] );
-        } )
-        .y1( function( d ) {
-            return y( d[ 1 ] );
-        } );
-
-    return { area, layers, z }
-};
-
-const stackMax = function(layer) {
-    return d3.max(layer, function(d) { return d[1]; });
-};
-
-const stackMin = function(layer) {
-    return d3.min(layer, function(d) { return d[0]; });
-};
+// TODO 1. Add button ot transition between layers
+// TODO 2. Verify that SVG is not getting updated when the component gets new props
 
 class AppContainer extends React.Component {
     constructor( props ) {
         super( props );
-        let svgParams = {
-            width: 960,
-            height: 500
+        this.state = {
+            height: 500,
+            k: 10,
+            m: 200,
+            n: 20,
+            width: 960
         };
-        this.state = _.merge(
-            { },
-            svgParams,
-            generateStreamData( svgParams.width, svgParams.height )
-        );
     }
 
     render() {
@@ -89,11 +23,11 @@ class AppContainer extends React.Component {
             <div>
                 <h1>Experiments in React and D3</h1>
                 <Streamograph
-                    area={ this.state.area }
                     height={ this.state.height }
-                    layers={ this.state.layers }
+                    k={ this.state.k }
+                    m={ this.state.m }
+                    n={ this.state.n }
                     width={ this.state.width }
-                    z={ this.state.z }
                 />
             </div>
         );
