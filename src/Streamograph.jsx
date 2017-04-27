3e4de2ebe1f7b81076ee3390c281213e5f4ba450
Phil Gates-Shannon <phil.gs@gmail.com>
2017-04-27 16:31:59 -0400

    Add skeleton Streamograph component

---
+++ src/Streamograph.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Streamograph extends React.Component {
+    render() {
+        return (
+            <div>Streamograph</div>
+        );
+    }
+}
+
+Streamograph.propTypes = {
+
+};
+
+export default Streamograph;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Streamograph from './Streamograph';
 import './stylesheets/app.scss';
 
 class AppContainer extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello Awesome World!</h1>
-                <pre>This is monospaced text.</pre>
+                <h1>Experiments in React and D3</h1>
+                <Streamograph/>
             </div>
         );
     }
